1263b9c08751a90f66bdc81b7d5677b9087ee794
Michael Lai <themichaellai@gmail.com>
2014-12-20 07:04:43 +0800

    Initial Commit

---
+++ chron/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chron
+//
+//  Created by Michael Lai on 12/20/14.
+//  Copyright (c) 2014 Duke Chronicle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
